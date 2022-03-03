f44716ce8e71af762b8fd3ae9ac7181879630a32
Zachary <zvcoding@gmail.com>
2022-03-02 19:21:30 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+
+  let testArrayOne = [1,2,1];
+  let testArrayTwo = [2,4,8,8];
+  let testArrayThree = [4,4,1];
+
+  function init(){
+     return same(testArrayOne, testArrayThree)
+  }
+
+  function same(arrayOne,arrayTwo){
+  let arrayOneS = squared(arrayOne)
+  let objectOne = ObjectMaker(arrayOneS)
+  let objectTwo = ObjectMaker(arrayTwo)
+  return loopThroughKeys(objectOne,objectTwo)
+  }
+
+  function loopThroughKeys(objectA,objectB){
+    let trueCounter = 0;
+    for(const property in objectA){
+      if(objectA[property] === objectB[property]){
+        trueCounter ++
+      }
+    }
+      return (trueCounter === (Object.keys(objectA).length))
+  }
+
+  function squared(arrayZ){
+    return arrayZ.map((number)=>number*number)
+  }
+
+  function ObjectMaker(arrayZ){
+    let objectZ = {}
+    for(let i = 0;i<arrayZ.length;i++){
+      if(!(objectZ[`${arrayZ[i]}`])){
+        objectZ[`${arrayZ[i]}`]=1;
+      } else {
+        objectZ[`${arrayZ[i]}`]+=1;
+      }
+    }
+    return objectZ
+  }
+
+
+  console.log(init())
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {init()} */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,95 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App.js'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Anagrams //
+
+// let test1 = "ddog"
+// let test2 = "godd"
+
+// function objectMaker(word){
+//   let letters = word.split('')
+//   let objectA = {}
+//   for(let i = 0; i<letters.length;i++){
+//     if((!(objectA[`${letters[i]}`]))){
+//       objectA[`${letters[i]}`] = 1
+//     }else{
+//       objectA[`${letters[i]}`]+=1
+//     }
+
+//   }
+//   return objectA
+// }
+
+// function anagrams(word1,word2){
+//   let objectA = objectMaker(word1);
+//   let objectB = objectMaker(word2);
+//   for(const property in objectA){
+//     if(objectA[property] !== objectB[property])
+//     {return false}
+//   }
+//   return true
+// }
+
+// Pointers //
+
+// Left and Right Pointers
+
+
+// function sumZero(orderedArray){
+
+//   for(let i = 0;i<orderedArray.length;i++){
+
+//     for(let j = i+1; j<orderedArray.length;j++){
+//       if(orderedArray[i]+orderedArray[j] === 0){
+//         return [orderedArray[i],orderedArray[j]]
+//       }
+//     }
+
+//   }
+// }
+
+// console.log(sumZero(testCase3))
+
+
+const testCase1 = [1,1,2,3,3]
+const testCase2 = [1,2,3,4,4,4,7,7,12,12,12,13]
+const testCase3 = []
+const testCase4 = [-2,-1,-1,0,1]
+const testCase5 = [1,2,3,4,4,4]
+
+function countUniqueNumbers(arrayZ){
+  let firstPointer = 0;
+  let secondPointer = 1;
+
+
+  while(secondPointer<arrayZ.length){
+    if(arrayZ[firstPointer] === arrayZ[secondPointer])
+    {
+      secondPointer ++
+    } else{
+      firstPointer ++
+      arrayZ[firstPointer] = arrayZ[secondPointer]
+      secondPointer ++
+    }
+  }
+
+
+
+ return firstPointer+1
+
+}
+
+console.log(countUniqueNumbers(testCase1))
+console.log(countUniqueNumbers(testCase2))
+console.log(countUniqueNumbers(testCase3))
+console.log(countUniqueNumbers(testCase4))
+console.log(countUniqueNumbers(testCase5))
